9c3ece1dffbcc54402cda87bfa4328450df976b4
liambabe <liambabes13@gmail.com>
2020-12-19 22:16:59 +1100

    Implementing user interface

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/*
+ * Entry point for the watch app
+ */
+import document from "document";
+
+//Button assignment
+let upBtn = document.getElementById("volumeUpBtn");
+let downBtn = document.getElementById("volumeDownBtn");
+let onBtn = document.getElementById("onBtn");
+let offBtn = document.getElementById("offBtn");
