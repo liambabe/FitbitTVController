ad7976833458df808dae5e9b389e92a60abd1217
liambabe <liambabes13@gmail.com>
2020-12-23 00:13:05 +1100

    implementing companion and basic communication between companion and app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
-/*
- * Entry point for the watch app
- */
 import document from "document";
+import { SocketCommunication } from "./socketCommunication";
 
 //Button assignment
 let upBtn = document.getElementById("volumeUpBtn");
 let downBtn = document.getElementById("volumeDownBtn");
 let onBtn = document.getElementById("onBtn");
 let offBtn = document.getElementById("offBtn");
+
+let socketCommunicator = new SocketCommunication();
+
+upBtn.onactivate = () => socketCommunicator.sendMessage("Volume Up Pressed");
+downBtn.onactivate = () => socketCommunicator.sendMessage("Volume Down Pressed");
+onBtn.onactivate = () => socketCommunicator.sendMessage("On Pressed");
+offBtn.onactivate = () => socketCommunicator.sendMessage("Off Pressed");
---
+++ app/socketCommunication.js
@@ -0,0 +1,23 @@
+import * as messaging from "messaging";
+
+export class SocketCommunication {
+
+
+    constructor() {
+
+        // Listen for the onerror event
+        messaging.peerSocket.onerror = function(err) {
+            console.log("Connection error: " + err.code + " - " + err.message);
+        }
+        
+    }
+
+    // Send a message to the peer
+    export sendMessage(data) {
+        if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+            // Send the data to peer as a message
+            messaging.peerSocket.send(data);
+        }
+    }
+
+}
